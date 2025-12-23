d9f6a73a3c6c713104b44a4e6021528b767257ae
giantmountain-taesan <galleryadellary@gmail.com>
2025-12-23 13:04:22 +0700

    Refactor data loading and KMeans training code

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,8 +3,7 @@ import mlflow
 from sklearn.cluster import KMeans
 
 # 1. Load data 
-file_path = 'Membangun_model/StudentPerformanceFactors_preprocessing.csv'
-df = pd.read_csv(file_path)
+df = pd.read_csv('StudentPerformanceFactors_preprocessing.csv')
 
 # Pastikan hanya mengambil kolom angka untuk KMeans
 X = df.select_dtypes(include=['float64', 'int64'])
@@ -17,4 +16,5 @@ n_clusters = 3
 with mlflow.start_run(run_name="KMeans_Baseline"):
     kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
     kmeans.fit(X)
+
     print(" Berhasil! Sekarang ketik 'mlflow ui' di terminal.")
