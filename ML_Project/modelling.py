71478fcddfcaaf2353d06705dd5c0caa3e3dc407
giantmountain-taesan <galleryadellary@gmail.com>
2025-12-24 01:28:34 +0700

    Remove unnecessary comments and clean up code

--- ML_Project/modelling.py
+++ ML_Project/modelling.py
@@ -8,8 +8,7 @@ X = df.select_dtypes(include=['float64', 'int64'])
 
 # 2. Training K-Means
 n_clusters = 3
-
-# PAKAI INI SAJA: Tanpa nama, tanpa ID, biar MLflow buat yang benar-benar baru
+ 
 with mlflow.start_run():
     kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
     kmeans.fit(X)
@@ -19,3 +18,4 @@ with mlflow.start_run():
     mlflow.log_metric("inertia", kmeans.inertia_)
 
     print("Berhasil!")
+
