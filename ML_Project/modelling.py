5d03fc13fffdb6bc8f09a49baef72aa13929601f
giantmountain-taesan <galleryadellary@gmail.com>
2025-12-24 00:29:33 +0700

    Add files via upload

---
+++ ML_Project/modelling.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import mlflow 
+from sklearn.cluster import KMeans
+
+# 1. Load data 
+df = pd.read_csv('StudentPerformanceFactors_preprocessing.csv')
+
+# Pastikan hanya mengambil kolom angka untuk KMeans
+X = df.select_dtypes(include=['float64', 'int64'])
+
+# 2. Aktifkan Autolog
+mlflow.autolog()
+
+# 3. Training K-Means
+n_clusters = 3
+with mlflow.start_run(run_name="KMeans_Baseline"):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
+    kmeans.fit(X)
+
+    print(" Berhasil! Sekarang ketik 'mlflow ui' di terminal.")
