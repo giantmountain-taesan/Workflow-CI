dc617b67d352ebb50f549b8b4bdc1beb69286bd2
giantmountain-taesan <galleryadellary@gmail.com>
2025-12-24 01:20:43 +0700

    Refactor KMeans training code and comments
    Updated comments for clarity and added manual logging notes.

--- ML_Project/modelling.py
+++ ML_Project/modelling.py
@@ -4,17 +4,17 @@ from sklearn.cluster import KMeans
 
 # 1. Load data 
 df = pd.read_csv('StudentPerformanceFactors_preprocessing.csv')
-
-# Mengambil kolom angka untuk KMeans
 X = df.select_dtypes(include=['float64', 'int64'])
 
 # 2. Training K-Means
-n_clusters = 3 
+n_clusters = 3
 
+# PAKAI INI SAJA: Tanpa nama, tanpa ID, biar MLflow buat yang benar-benar baru
 with mlflow.start_run():
     kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
     kmeans.fit(X)
- 
+    
+    # Manual logging untuk syarat Advanced
     mlflow.log_param("n_clusters", n_clusters)
     mlflow.log_metric("inertia", kmeans.inertia_)
 
